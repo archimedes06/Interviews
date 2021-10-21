96248966d11049948a6c921175e92979ddb0d9b2
Brock Bose <brock.bose@charter.com>
2021-10-21 17:12:10 -0600

    fixed error in python function3.py

--- code/python/function3.py
+++ code/python/function3.py
@@ -1,11 +1,10 @@
 from math import floor
 from itertools import groupby
- 
+
 def function3(l, n):
-    assert(len(l) > 0 and n > 0)
- 
-    min = min(l)
-    max = max(l)
-    d = (max - min) / n
-    values = [(floor((l[i] - min) / d)) for i in range(len(l))]
-    return {(key, len(list(group))) for key, group in groupby(values)}
+  assert(len(l) > 0 and n > 0)
+  minimum = min(l)
+  maximum = max(l)
+  d = (maximum - minimum) / n
+  values = [(floor((l[i] - minimum) / d)) for i in range(len(l))]
+  return [(minimum + d*(key+0.5), len(list(group))) for key, group in groupby(sorted(values))]
